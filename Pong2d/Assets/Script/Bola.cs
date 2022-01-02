4b4ecb4ceda3502686ce84b9abbdaeb1009dd183
Iann Leles <iann.marcos97@gmail.com>
2022-01-02 19:04:58 -0300

    Ajuste de fisica da bolinha, e colisao com o teto e chao

---
+++ Pong2d/Assets/Script/Bola.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bola : MonoBehaviour
+{
+
+    public float velocidadeDaBola;
+
+    public Rigidbody2D oRigidyBody2d;
+
+    public AudioSource somDaBola;
+
+    public float direcaoAleatoriaX;
+
+    public float direcaoAleatoriaY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MoverBola();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void MoverBola()
+    {
+        oRigidyBody2d.velocity = new Vector2(velocidadeDaBola, velocidadeDaBola);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        somDaBola.Play();
+        oRigidyBody2d.velocity += new Vector2(direcaoAleatoriaX, direcaoAleatoriaY);
+
+    }
+}
