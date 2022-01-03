400c42839bed4d3ee8a2833132afb3c3f60b9c49
Iann Leles <iann.marcos97@gmail.com>
2022-01-02 23:24:13 -0300

    Add Reiniciar partida e Sair do jogo com as teclas esc e R

--- Pong2d/Assets/Script/GameManager.cs
+++ Pong2d/Assets/Script/GameManager.cs
@@ -1,6 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
 
 public class GameManager : MonoBehaviour
 {
@@ -9,25 +12,60 @@ public class GameManager : MonoBehaviour
 
     public int pontuacaoDoJogador2;
 
+    public Text textoDePontuacao;
+
+    public AudioSource somDoGol;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Cursor.visible = false;
+
+        pontuacaoDoJogador1 = 0;
+
+        pontuacaoDoJogador2 = 0;
+        textoDePontuacao.text = pontuacaoDoJogador1 + " X " + pontuacaoDoJogador2;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            ReiniciarPartida();
+        }
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            SairDoJogo();
+        }
     }
 
 
     public void AumentarPontuacaoDoJogador1()
     {
         pontuacaoDoJogador1 += 1;
+        AtualizarTextoDePontuacao();
     }    
     public void AumentarPontuacaoDoJogador2()
     {
         pontuacaoDoJogador2 += 1;
+        AtualizarTextoDePontuacao();
+    }
+
+    public void AtualizarTextoDePontuacao()
+    {
+        textoDePontuacao.text = pontuacaoDoJogador1 + " X " + pontuacaoDoJogador2;
+        somDoGol.Play();
+    }
+
+    private void ReiniciarPartida()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    private void SairDoJogo()
+    {
+        Application.Quit();
+        Debug.Log("Saiu do jogo");
     }
 }
